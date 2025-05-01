91f8a203e65e7ba8dd6e561c36923487bd8a9812
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-05-01 11:04:08 +0700

    Fix: Refactor admin

--- app/admin/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '@/components/layout/LoginForm';
-
-export default function LoginPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-blue-600 px-4 py-10 sm:px-6">
-      <LoginForm/>
-    </main>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import LoginForm from "@/components/layout/LoginForm"
 
 export default function Home() {
   return (
-    <></>
-  );
-}                            
+    <main className='min-h-screen flex items-center justify-center bg-blue-600 px-4 py-10 sm:px-6'>
+      <LoginForm />
+    </main>
+  )
+}
